5cf18ac01643435800ebbae3791850370690c18f
Alex Samih-zade <galutziogen@gmail.com>
2022-12-03 22:49:20 +0200

    bg pic, search

---
+++ assets/globals.js
@@ -0,0 +1,52 @@
+const _SPINNER = `
+<div class="text-center">
+<div class="spinner-border" role="status" style="width: 3rem; height: 3rem;">
+<span class="visually-hidden">Loading...</span>
+</div>
+</div>`;
+
+const _SPINNER_BTN = `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+<span class="visually-hidden">Loading...</span>`;
+
+
+
+// ========================================= DEBOUNCER ========================================= \\
+
+
+// Originally inspired by  David Walsh (https://davidwalsh.name/javascript-debounce-function)
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// `wait` milliseconds.
+const debounce = (func, wait) => {
+    let timeout;
+  
+    return function executedFunction(...args) {
+      const later = () => {
+        clearTimeout(timeout);
+        func(...args);
+      };
+  
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+    };
+  };
+
+// function getRandomId(idLength=10){
+//     let id = '';
+//     let randomIndex;
+//     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%&=+<>?~';
+//     for (let i = 0; i<idLength;i++){
+//         randomIndex = Math.floor(Math.random() * chars.length);
+//         id += chars[randomIndex]; 
+//     }
+//     return id;     
+// }
+
+
+const _SEARCHFAIL = `<main class="container">
+<div class="bg-light p-5 rounded">
+  <h1>No Luck taday :(</h1>
+  <p class="lead">Try harder next time - no one gets ride for free.</p>
+</div>
+</main>`
--- assets/index.js
+++ assets/index.js
@@ -1,47 +1,48 @@
 $(function() {
 
-const _SPINNER = `
-<div class="text-center">
-<div class="spinner-border" role="status">
-    <span class="visually-hidden">Loading...</span>
-</div>
-</div>`;
-
-
+let coinsLocaleCopy = [];
 $('.nav-item').click(changeAppContent);
-
 $("#root" ).on("click", ".collapseBtn", function() { // adds click events to each .collapseBtn in root div, and toggles collapse.
-    $(this).next().collapse('toggle');
-});
+  createCoinInfo( $(this).next() );
+}); 
 
-renderHomePage();
+showHomePage();
 
-function renderHomePage(){
-    
-    fetchData('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1',renderLandingCoinList,'#root');
-    console.log(data);
-}
+// -------------------------- home page -------------------------- //
+async function showHomePage() {
 
-function fetchData(url,callback,asyncElement){
-    
-    $(asyncElement).html('').append(_SPINNER);
-    fetch(url)
-    .then((response) => response.json())
-    .then((data) => callback(data));
+  $('#root').html('').append(_SPINNER); // insert spinner
+
+  try{
+    const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1');
+
+    const data = await res.json();
+
+    const homepage =  renderCoinList(data);
+
+    //Object.assign(coinsLocaleCopy, data);
+    coinsLocaleCopy = Array.from(data)
+
+    render('#root', homepage);
   
+  }catch(error){
+    
+    render('#root', error);
+  
+  }
+   
 }
 
-function renderLandingCoinList(data){
-    const gridList = $("<div>").addClass("row");
+function renderCoinList(data){
+    const cardList = $("<div>").addClass("row");
     for(const coin of data){
-        gridList.append(createCard(coin));
+      cardList.append(createCard(coin));
     }
-    $('#root').html('').append(gridList);
+    return cardList;   
 }
 
 function createCard(coin){
-    console.log(coin.symbol)
-    const id = getRandomId();
+    //const id = getRandomId();
     const card = `
         <div class="col-xs-12 col-sm-6 col-md-4 col-xl-3">
           <div class="card border-dark mb-3" style="max-width: 18rem;">
@@ -54,11 +55,7 @@ function createCard(coin){
             <div class="card-body text-dark">
               <p class="card-text">${coin.name}</p>
               <button class="btn btn-primary collapseBtn">More info</button>
-              <div class="collapse" id="${id}">
-                <img class="float-end" src="https://assets.coingecko.com/coins/images/1/small/bitcoin.png?1547033579" alt="">
-                <div>USD price: 5$</div>
-                <div>EUR price: 8</div>
-                <div>NIS price: 15.2</div> 
+              <div class="collapse" id="${coin.id}">
               </div>
             </div>
           </div>
@@ -69,110 +66,67 @@ function createCard(coin){
 
 }
 
-function getRandomId(idLength=10){
-    let id = '';
-    let randomIndex;
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%&=+<>?~';
-    for (let i = 0; i<idLength;i++){
-        randomIndex = Math.floor(Math.random() * chars.length);
-        id += chars[randomIndex]; 
-    }
-    return id;     
+async function createCoinInfo(element){
+  
+  if(!element.hasClass("show")){ element.prev().append(_SPINNER_BTN); }
+  
+  const res = await fetch('https://api.coingecko.com/api/v3/coins/' + element.attr('id'));
+
+  const data = await res.json();
+
+  const infoElement = `
+    <img class="float-end" style="width:70px" src="${data.image.small}" alt="${data.name} image">
+    <div>USD price: ${data.market_data.current_price.usd} $</div>
+    <div>EUR price: ${data.market_data.current_price.eur} &#8364;</div>
+    <div>NIS price: ${data.market_data.current_price.ils} &#8362;</div> 
+    `;
+
+    render(element, infoElement)
+    element.collapse('toggle');
+    
+    element.prev().empty().text('More info');
+
+    
+}
+
+// -------------------------- util functions -------------------------- //
+
+function render(container, element){
+  
+  $(container).html('').append(element);
+
 }
 
 function changeAppContent(){
-    console.log($(this)[0].outerText);
- }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  const page = $(this)[0].outerText;
+  if(page == "Home"){
+    console.log('Go home');
+    showHomePage();
+  } else if(page =="Live Reports"){
+    console.log('Go to live');
+  } else {
+    console.log('Go to about me');
+  }
+}
+
+const search = debounce(function() {
+  const filteredArr = [];
+  const searchValue = $('#search').val().toLowerCase(); // search input
+  
+  for(const coin of coinsLocaleCopy){
+    
+    const coinName = coin.name.toLowerCase();
+    const coinSymbol = coin.symbol.toLowerCase();
+
+    if (coinName.startsWith(searchValue) || coinSymbol.startsWith(searchValue)){
+      filteredArr.push(coin);
+    }
+  }
+
+  let searchResult = renderCoinList(filteredArr);
+  if(filteredArr.length == 0){searchResult = _SEARCHFAIL;} // If no search result - print msg
+  render('#root', searchResult);
+}, 500);
+$('#search').on('keyup focusout change', search);
 
 });
-
