a2865842aab6da50a3e3d8c15f3c604f5a2033a7
Alex Samih-zade <galutziogen@gmail.com>
2022-12-05 23:18:41 +0200

    Final functions for modal and togglers

--- assets/index.js
+++ assets/index.js
@@ -1,14 +1,7 @@
-    
 const myModal = new bootstrap.Modal(document.getElementById('modal'), {keyboard: false})
 let coinsLocaleCopy = [];
 let chartlist = [];
 $('.nav-item').click(changeAppContent);
-
-$("#root" ).on("change", "input[role='switch']", function() {
-    const selectedId = $(this).parent().parent().next().find('div').attr("id");
-    const isChecked = $(this).prop("checked");
-    handleAddToChartlist(isChecked, selectedId,this); 
-});
 
 $("#root" ).on("click", ".collapseBtn", function() { // adds click events to each .collapseBtn in root div, and toggles collapse.
   createCoinInfo( $(this).next() );
@@ -24,7 +17,7 @@ async function showHomePage() {
   try{
     const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1');
     const data = await res.json();
-    const homepage =  renderCoinList(data);
+    const homepage =  renderCoinList(data, createCard);
     coinsLocaleCopy = Array.from(data)
     render('#root', homepage);
   }catch(error){
@@ -33,10 +26,10 @@ async function showHomePage() {
    
 }
 
-function renderCoinList(data){
+function renderCoinList(data,builderCallback){
     const cardList = $("<div>").addClass("row");
     for(const coin of data){
-      cardList.append(createCard(coin));
+      cardList.append(builderCallback(coin));
     }
     return cardList;   
 }
@@ -45,14 +38,15 @@ function createCard(coin){
     //const id = getRandomId();
     const card = `
         <div class="col-xs-12 col-sm-6 col-md-4 col-xl-3">
-          <div class="card border-dark mb-3" style="max-width: 18rem;">
+          <div class="card border-dark mb-3 mx-auto" style="max-width: 18rem;">
             <div class="card-header">
               <span>${coin.symbol.toUpperCase()}</span>
               <div class="form-check form-switch float-end">
-                <input class="form-check-input" type="checkbox" role="switch">
+                <input class="form-check-input" coin-symbol="${coin.symbol}" type="checkbox" role="switch">
               </div>
             </div>
             <div class="card-body text-dark">
+              <img class="float-end" style="width:70px" src="${coin.image}" alt="image">
               <p class="card-text">${coin.name}</p>
               <button class="btn btn-primary collapseBtn">More info</button>
               <div class="collapse" id="${coin.id}">
@@ -66,14 +60,13 @@ function createCard(coin){
 
 }
 
-async function createCoinInfo(element){ // THIS FUNC SHOULD BE REFACTORED  
+async function createCoinInfo(element){ 
   
   if(!element.hasClass("show")){ element.prev().append(_SPINNER_BTN); }
 
   const res = await fetch('https://api.coingecko.com/api/v3/coins/' + element.attr('id'));
   const data = await res.json();
   const infoElement = `
-    <img class="float-end" style="width:70px" src="${data.image.small}" alt="${data.name} image">
     <div>USD price: ${data.market_data.current_price.usd} $</div>
     <div>EUR price: ${data.market_data.current_price.eur} &#8364;</div>
     <div>NIS price: ${data.market_data.current_price.ils} &#8362;</div> 
@@ -84,7 +77,6 @@ async function createCoinInfo(element){ // THIS FUNC SHOULD BE REFACTORED
     element.prev().empty().text('More info');
 }
 
-// -------------------------- util functions -------------------------- //
 
 function render(container, element){
   
@@ -104,7 +96,7 @@ function changeAppContent(){
   }
 }
 
-const search = debounce(function() { // THIS SHOULD BE FIXED WITH ARR METHOD FOR FILTER !!! 
+const search = debounce(function() { 
   const filteredArr = [];
   const searchValue = $('#search').val().toLowerCase(); // search input
   
@@ -125,28 +117,92 @@ const search = debounce(function() { // THIS SHOULD BE FIXED WITH ARR METHOD FOR
 $('#search').on('keyup focusout change', search);
 
 
+// 5-selector logics
 
+$("#root" ).on("change", "input[role='switch']", function() {
+  const selectedId = $(this).parent().parent().next().find('div').attr("id");
+  const isChecked = $(this).prop("checked");
+  handleAddToChartlist(isChecked, selectedId,this); 
+});
 
 function handleAddToChartlist(isChecked, selectedId, togglerSwitch){
     
     const coinIndexInLocale = coinsLocaleCopy.findIndex((coin)=>{return coin.id == selectedId})
     
-    if(isChecked && chartlist.length<2){    
+    // add to chart list
+    if(isChecked && chartlist.length<5){    
         chartlist.push(coinsLocaleCopy[coinIndexInLocale]);
     } 
-        
-    else if(isChecked && chartlist.length >= 2){ 
+    
+    // Current selection is above selection limit - show modal to user
+    else if(isChecked && chartlist.length >=5){ 
         //render choise modal
         $(togglerSwitch).prop("checked",false);
-        const modalContent =  renderCoinList(chartlist);
+        localStorage.setItem('idInQueue', selectedId);
+        const modalContent =  renderCoinList(chartlist,createModalCard);
         render('#modalContent', modalContent);
         myModal.show();
+
     } 
     
+    // Remove unchecked element from chart list
     else {
         const indexToRemove = chartlist.findIndex((coin)=>{return coin.id == selectedId});
         chartlist.splice(indexToRemove, 1);
     }
 
-    console.log(chartlist);
+    // console.log(chartlist);
+}
+
+function createModalCard(coin){
+  const modalCard = `
+  <div class="col-xs-6 col-sm-6 col-md-4 col-xl-3">
+    <div class="card border-dark mb-3 mx-auto" style="max-width: 18rem;">
+      <div class="card-body">
+        <img class="float-end" style="width:50px" src="${coin.image}" alt="image">
+        <span>${coin.symbol.toUpperCase()}</span>
+        <div class="form-check form-switch">
+          <input class="form-check-input" type="checkbox" role="switch" checked>
+        </div>
+      </div>
+    </div>
+  </div>
+`;
+
+  return modalCard;
+
+}
+
+// on user 'save' in modal dialog
+function updateOnModalSave(el){ 
+  
+  const itemsToInclude = $(el).parent().prev().find('input:checked').parent().prev();
+  const itemsToExclude = $(el).parent().prev().find('input:not(:checked)').parent().prev();
+  
+  // clear excluded togglers
+  for(const item of itemsToExclude){
+    const symbol = item.innerHTML.toLowerCase();
+    $(`input[coin-symbol="${symbol}"]`).prop("checked",false);
+  }
+
+  chartlist.length = 0; //reset chart list
+
+  for(const item of itemsToInclude){ // Create 
+    const symbol = item.innerHTML.toLowerCase();
+    chartlist.push(coinsLocaleCopy.find(element => element.symbol == symbol));
+    $(`input[coin-symbol="${symbol}"]`).prop("checked",true);
+
+  }
+  
+  if(chartlist.length < 5){
+
+    chartlist.push(coinsLocaleCopy.find(element => element.id == localStorage.getItem('idInQueue')));
+    const addedSymbol = chartlist[chartlist.length-1].symbol;
+    $(`input[coin-symbol="${addedSymbol}"]`).prop("checked",true);
+  }
+
+
+  myModal.hide();
+
+
 }
