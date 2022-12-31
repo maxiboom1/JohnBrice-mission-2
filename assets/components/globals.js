a43adc62f54a7026ad14949541d0cba7869cbe6b
Alex Samih-zade <galutziogen@gmail.com>
2023-01-01 00:06:33 +0200

    final

--- assets/components/globals.js
+++ assets/components/globals.js
@@ -23,22 +23,18 @@ const _ABOUT = `
     <div class="image d-flex flex-column justify-content-center align-items-center">
 
         <button class="btn btn-secondary"> <img src="assets/img/profile.png" height="100" width="100" /></button>
-        <span class="name mt-3">Alex Samih-zade</span> <span class="idd">@Maxiboom1</span>
-        
+        <span class="name mt-3">Alex Samih-zade</span><br>
+        <span class="idd">@Maxiboom1</span>
         <div class="d-flex flex-row justify-content-center align-items-center mt-3">
-            <span class="number">5 <span class="follow">Happy JS years</span></span> </div>
+            <span class="number">5 <span class="follow">Happy JS coding years</span></span> </div>
         <div class=" d-flex mt-2"> 
-        <button class="btn1 btn-dark" onclick="test()">Show me more</button> 
-        </div>
         <div class="text mt-3">
             <span>Alex is a Full Stack Web Developer with an emphasis on complex, challenging JS coding.<br><br>
             </span>
-        </div>
-        
-        <div class="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center">
-            <span><i class="fa fa-github"></i></span> 
-            <span><i class="fa fa-facebook-f"></i></span>
-        </div>            
+        </div>        
     </div>
 </div>
-</div> `;
+
+
+</div> 
+`;
